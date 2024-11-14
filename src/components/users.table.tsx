623d2f768cca3a1a4da7e15b52e3e7e3c4f9a769
vuvansy <vuvansy3001@gmail.com>
2024-11-14 13:34:06 +0700

    Cài đặt React Query,Fetch Data, useQuery Hook, React Query Devtool, Query parameter

--- src/components/users.table.tsx
+++ src/components/users.table.tsx
@@ -1,15 +1,21 @@
-import Table from 'react-bootstrap/Table';
-import { useState, forwardRef } from 'react';
-import Button from 'react-bootstrap/Button';
-import UserCreateModal from './modal/user.create.modal';
-import UserEditModal from './modal/user.edit.modal';
-import UserDeleteModal from './modal/user.delete.modal';
-import UsersPagination from './pagination/users.pagination';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Popover from 'react-bootstrap/Popover';
+import Table from "react-bootstrap/Table";
+import { useState, forwardRef } from "react";
+import Button from "react-bootstrap/Button";
+import UserCreateModal from "./modal/user.create.modal";
+import UserEditModal from "./modal/user.edit.modal";
+import UserDeleteModal from "./modal/user.delete.modal";
+import UsersPagination from "./pagination/users.pagination";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
+import { useQuery } from "@tanstack/react-query";
+
+interface IUser {
+    id: number;
+    name: string;
+    email: string;
+}
 
 function UsersTable() {
-
     const [isOpenCreateModal, setIsOpenCreateModal] = useState<boolean>(false);
 
     const [isOpenUpdateModal, setIsOpenUpdateModal] = useState<boolean>(false);
@@ -17,58 +23,94 @@ function UsersTable() {
 
     const [isOpenDeleteModal, setIsOpenDeleteModal] = useState<boolean>(false);
 
-    const users = [
-        {
-            "id": 1,
-            "name": "Eric",
-            "email": "eric@gmail.com"
-        },
-        {
-            "id": 2,
-            "name": "Hỏi Dân IT",
-            "email": "hoidanit@gmail.com"
-        },
-        {
-            "id": 3,
-            "name": "Hỏi Dân IT",
-            "email": "admin@gmail.com"
-        }
-    ]
+    // const users = [
+    //     {
+    //         "id": 1,
+    //         "name": "Eric",
+    //         "email": "eric@gmail.com"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "name": "Hỏi Dân IT",
+    //         "email": "hoidanit@gmail.com"
+    //     },
+    //     {
+    //         "id": 3,
+    //         "name": "Hỏi Dân IT",
+    //         "email": "admin@gmail.com"
+    //     }
+    // ]
 
     const handleEditUser = (user: any) => {
         setDataUser(user);
         setIsOpenUpdateModal(true);
-    }
+    };
 
     const handleDelete = (user: any) => {
         setDataUser(user);
         setIsOpenDeleteModal(true);
-    }
+    };
 
     const PopoverComponent = forwardRef((props: any, ref: any) => {
         const { id } = props;
+        const { isPending, error, data } = useQuery({
+            queryKey: ["fetchUser", id],
+            queryFn: (): Promise<IUser> =>
+                fetch(`http://localhost:8000/users/${id}`).then((res) =>
+                    res.json()
+                ),
+        });
 
+        const getBody = () => {
+            if (isPending) {
+                return "Loading detail...";
+            }
+            if (error) return "An error has occurred: " + error.message;
+            if (data) {
+                return (
+                    <>
+                        <div>ID = {id}</div>
+                        <div>Name = {data?.name}</div>
+                        <div>Email = {data?.email}</div>
+                    </>
+                );
+            }
+        };
+        console.log(isPending, error, data);
         return (
-
             <Popover ref={ref} {...props}>
                 <Popover.Header as="h3">Detail User</Popover.Header>
-                <Popover.Body>
-                    <div>ID = {id}</div>
-                    <div>Name = ?</div>
-                    <div>Email = ?</div>
-                </Popover.Body>
+                <Popover.Body>{getBody()}</Popover.Body>
             </Popover>
-        )
-    })
+        );
+    });
 
+    const { isPending, error, data } = useQuery({
+        queryKey: ["fetchUser"],
+        queryFn: (): Promise<IUser[]> =>
+            fetch("http://localhost:8000/users").then((res) => res.json()),
+    });
+
+    if (isPending) return "Loading...";
+
+    if (error) return "An error has occurred: " + error.message;
 
     return (
         <>
-            <div style={{ display: "flex", justifyContent: "space-between", margin: "15px 0" }}>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    margin: "15px 0",
+                }}
+            >
                 <h4>Table Users</h4>
-                <Button variant="primary"
+                <Button
+                    variant="primary"
                     onClick={() => setIsOpenCreateModal(true)}
-                >Add New</Button>
+                >
+                    Add New
+                </Button>
             </div>
             <Table striped bordered hover>
                 <thead>
@@ -80,14 +122,18 @@ function UsersTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    {users?.map(user => {
+                    {data?.map((user) => {
                         return (
                             <tr key={user.id}>
-                                <OverlayTrigger trigger="click" placement="right"
+                                <OverlayTrigger
+                                    trigger="click"
+                                    placement="right"
                                     rootClose
                                     overlay={<PopoverComponent id={user.id} />}
                                 >
-                                    <td><a href='#'>{user.id}</a></td>
+                                    <td>
+                                        <a href="#">{user.id}</a>
+                                    </td>
                                 </OverlayTrigger>
 
                                 <td>{user.name}</td>
@@ -98,7 +144,8 @@ function UsersTable() {
                                         onClick={() => handleEditUser(user)}
                                     >
                                         Edit
-                                    </Button>&nbsp;&nbsp;&nbsp;
+                                    </Button>
+                                    &nbsp;&nbsp;&nbsp;
                                     <Button
                                         variant="danger"
                                         onClick={() => handleDelete(user)}
@@ -107,13 +154,11 @@ function UsersTable() {
                                     </Button>
                                 </td>
                             </tr>
-                        )
+                        );
                     })}
                 </tbody>
             </Table>
-            <UsersPagination
-                totalPages={0}
-            />
+            <UsersPagination totalPages={0} />
             <UserCreateModal
                 isOpenCreateModal={isOpenCreateModal}
                 setIsOpenCreateModal={setIsOpenCreateModal}
