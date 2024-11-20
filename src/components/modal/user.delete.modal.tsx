0f370b9dea3003b620bb48bfd5c8745c7ebe5765
vuvansy <vuvansy3001@gmail.com>
2024-11-20 14:21:18 +0700

    #56. Bài tập Delete User

--- src/components/modal/user.delete.modal.tsx
+++ src/components/modal/user.delete.modal.tsx
@@ -1,12 +1,42 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Spinner } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { toast } from "react-toastify";
+
+interface IUser {
+    id: number;
+}
 
 const UserDeleteModal = (props: any) => {
     const { dataUser, isOpenDeleteModal, setIsOpenDeleteModal } = props;
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: async (payload: IUser) => {
+            const res = await fetch(
+                `http://localhost:8000/users/${payload.id}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": " application/json",
+                    },
+                }
+            );
+
+            return res.json();
+        },
+        onSuccess: (data, variables, context) => {
+            toast("🦄 Wow so easy! Delete succeed!");
+            setIsOpenDeleteModal(false);
+            queryClient.invalidateQueries({ queryKey: ["fetchUser"] });
+        },
+    });
 
     const handleSubmit = () => {
+        mutation.mutate({ id: dataUser?.id });
         console.log({ id: dataUser?.id });
-    }
+    };
 
     return (
         <Modal
@@ -17,21 +47,36 @@ const UserDeleteModal = (props: any) => {
             onHide={() => setIsOpenDeleteModal(false)}
         >
             <Modal.Header closeButton>
-                <Modal.Title>
-                    Delete A User
-                </Modal.Title>
+                <Modal.Title>Delete A User</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                Delete the user: {dataUser?.email ?? ""}
-            </Modal.Body>
+            <Modal.Body>Delete the user: {dataUser?.email ?? ""}</Modal.Body>
             <Modal.Footer>
-                <Button
-                    variant='warning'
-                    onClick={() => setIsOpenDeleteModal(false)} className='mr-2'>Cancel</Button>
-                <Button onClick={() => handleSubmit()}>Confirm</Button>
+                {!mutation.isPending ? (
+                    <>
+                        <Button
+                            variant="warning"
+                            onClick={() => setIsOpenDeleteModal(false)}
+                            className="mr-2"
+                        >
+                            Cancel
+                        </Button>
+                        <Button onClick={() => handleSubmit()}>Confirm</Button>
+                    </>
+                ) : (
+                    <Button variant="primary" disabled>
+                        <Spinner
+                            as="span"
+                            animation="border"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"
+                        />
+                        <></> Deleting...
+                    </Button>
+                )}
             </Modal.Footer>
         </Modal>
-    )
-}
+    );
+};
 
 export default UserDeleteModal;
